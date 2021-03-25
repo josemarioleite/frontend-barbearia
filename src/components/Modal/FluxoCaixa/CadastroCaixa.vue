e310125f417fd248c826f9355d29dee0a8eb9dc2
José Mário Moreira Leite <31426252+josemarioleite@users.noreply.github.com>
2021-03-25 01:07:57 -0300

    ...

--- src/components/Modal/FluxoCaixa/CadastroCaixa.vue
+++ src/components/Modal/FluxoCaixa/CadastroCaixa.vue
@@ -1,79 +1,40 @@
 <template>
     <div class="col-12" style="padding: 5px">
-        <div class="col-md-6 row justify-end items-center q-pr-sm q-mb-sm">
-            <hr class="bg-primary linhaInclusao">
-            <q-btn @click="abreModalCadastro" flat round dense icon="add" color="white" class="bg-primary justify-start" />
-        </div>
         <q-list v-for="item in dados" :key="item.id" bordered class="q-mb-xs rounded-borders col-12">
-            <q-item clickable v-ripple class="col-12">
-                <q-item-section avatar class="col-md-1 col-xs-2">
-                    <q-avatar v-if="item.caixaAberto === 'S'" icon="price_check" size="40px" color="green" text-color="white" />
-                    <q-avatar v-else icon="payments" color="primary" text-color="white" />
-                </q-item-section>
-
-                <q-item-section avatar class="col-md-10 col-xs-8 espacoEntre">
-                    <div class="row">
-                        <q-item-label lines="1">{{item.nome}}</q-item-label>
-                        <div class="row col-12">
-                            <!-- <div class="col-9 text-grey-7 espacoTexto">{{item.observacao}}</div> -->
-                            <div class="col-9 text-grey-7 espacoTexto" caption v-if="item.updatedAt !== null">Última Abertura: {{new Date(item.updatedAt).toLocaleDateString('pt-BR')}}</div>
-                            <div class="col-9 text-grey-7 espacoTexto" caption v-else>Ainda não foi aberto</div>
-                        </div>
+            <q-slide-item ref="slideRef" @right="resetaSlider; deletarCaixa(item.id);" right-color="red">
+                <template v-slot:right>
+                    <div class="row items-center">Desativar caixar
+                        <q-icon right name="close" />
                     </div>
-                </q-item-section>
+                </template>
 
-                <q-item-section side class="col-md-1 col-xs-1">
-                    <q-icon v-if="item.caixaAberto === 'S'" name="brightness_1" color="green" right />
-                    <q-icon v-else name="brightness_1" color="red" right />
-                </q-item-section>
-            </q-item>
-        </q-list>
+                <q-item>
+                    <q-item-section avatar>
+                        <q-avatar class="bg-dark">
+                            <div class="text-white">{{item.nome.substr(0, 1)}}</div>
+                        </q-avatar>
+                    </q-item-section>
 
-        <q-dialog v-model="statusModal">
-            <q-card>
-                <q-toolbar>
-                    <q-toolbar-title>Cadastrar novo caixa</q-toolbar-title>
-                    <q-btn @click="fechaModal" flat round dense icon="close" v-close-popup />
-                </q-toolbar>
-
-                <q-card-section>
-                    <q-input :error="erroNomeCaixa" :error-message="erroMsgNomeCaixa" outlined v-model="nomeCaixa" label="Nome do caixa" stack-label dense class="q-mb-xs" />
-                    <q-input :error="erroObsCaixa" :error-message="erroMsgObsCaixa" outlined v-model="observacaoCaixa" label="Observação" stack-label dense class="q-mb-sm" />
-                    <div class="column items-center">
-                        <q-btn @click="salvaNovoCaixa" align="center" color="green" icon="add" label="Salvar" size="12px" />
-                    </div>
-                </q-card-section>
-            </q-card>
-        </q-dialog>
+                    <q-item-section>{{item.nome}}</q-item-section>
+                </q-item>
+            </q-slide-item>
+        </q-list>        
     </div>
 </template>
 
 <script>
-import { Get, Post } from 'src/utils/Conexao.js'
+import { Get, Delete } from 'src/utils/Conexao.js'
 
 export default {
     name: 'Cadastro-Caixa',
     data () {
         return {
-            statusModal: false,
-            erroNomeCaixa: false,
-            erroObsCaixa: false,
-            erroMsgNomeCaixa: '',
-            erroMsgObsCaixa: '',
-            nomeCaixa: '',
-            observacaoCaixa: '',
+            statusModal: false,            
+            headerClass: '',
             dados: []
         }
     },
     methods: {
-        abreModalCadastro() {
-            this.statusModal = !this.statusModal
-        },
-        fechaModal() {
-            this.statusModal = !this.statusModal
-            this.nomeCaixa = null
-            this.observacaoCaixa = null
-        },
         obterCaixas() {
             Get('v1/caixaoperador').then(res => {
                 if (parseInt(res.status) === 200) {
@@ -103,80 +64,63 @@ export default {
                     persistent: true
                 }).onOk(() => {
                     resolve()
+                }).onCancel(() => {
+                    reject()
                 })
             })
         },
-        validaCampos() {
-            return new Promise(resolve => {
-                var campoVazio = false
-                var texto = 'Este campo é obrigatório preencher'
-
-                if (this.nomeCaixa === '' || this.nomeCaixa === null) {
-                    this.erroNomeCaixa = true
-                    this.erroMsgNomeCaixa = texto
-                    campoVazio = true
-                } else {
-                    this.erroNomeCaixa = false
-                }
-
-                if (this.observacaoCaixa === '' || this.observacaoCaixa === null) {
-                    this.erroObsCaixa = true
-                    this.erroMsgObsCaixa = texto
-                    campoVazio = true
-                } else {
-                    this.erroObsCaixa = false
-                }
-
-                if (campoVazio === false) {
-                    resolve(true)
-                }
-            })
-        },
-        salvaNovoCaixa() {
-            this.validaCampos().then(() => {
-                this.confirmacao('Deseja incluir este novo caixa ?').then(() => {
-                    var caixa = Object()
-                    caixa.Nome = this.nomeCaixa
-                    caixa.Observacao = this.observacaoCaixa
-
-                    this.$q.notify({
-                        message: 'Carregando...',
-                        color: 'blue',
-                        timeout: 1000
-                    })
-
-                    setTimeout(() => {
-                        Post('v1/caixaoperador/cadastro', caixa).then(res => {
-                            if (res.data.status == true) {
-                                this.$q.notify({
-                                    message: res.data.msg,
-                                    color: 'green',
-                                    timeout: 2000
-                                })
-                                this.fechaModal()
-                                this.obterCaixas()
-                            } else {
-                                this.$q.notify({
-                                    message: res.data.msg,
-                                    color: 'red',
-                                    timeout: 2000
-                                })
-                            }
-                        }).catch(err => {
-                            this.$q.notify({
-                                message: 'Erro',
-                                caption: 'Não foi possível fazer inclusão, tente novamente mais tarde',
-                                color: 'red'
-                            })
-                            console.log(err)
+        deletarCaixa(id) {
+            this.confirmacao('Tem certeza que deseja desativar este caixa ?').then(() => {
+                setTimeout(() => {
+                    Delete('v1/caixaoperador/' + parseInt(id)).then(res => {
+                        this.$q.notify({
+                            message: 'Desativando...',
+                            color: 'blue',
+                            timeout: 1000
                         })
-                    }, 1000);
-                })
+
+                        if (res.data.status == true) {
+                            this.$q.notify({
+                                message: res.data.msg,
+                                color: 'green',
+                                timeout: 2000
+                            })
+                            this.obterCaixas()
+                        } else {
+                            this.$q.notify({
+                                message: res.data.msg,
+                                color: 'red',
+                                timeout: 2000
+                            })
+                        }
+                    }).catch(err => {
+                        this.$q.notify({
+                            message: 'Erro',
+                            caption: 'Não foi possível fazer inclusão, tente novamente mais tarde',
+                            color: 'red'
+                        })
+                        console.log(err)
+                    })
+                }, 2000);
+            }).catch(() => {
+                this.$refs.slideRef
             })
+        },
+        resetaSlider({reset}) {
+            this.finalize(reset)
+        },
+        finalize (reset) {
+            this.timer = setTimeout(() => {
+                reset()
+            }, 2000)
         }
+    },
+    beforeDestroy() {
+        clearTimeout(this.timer)
     },
     created() {
         this.obterCaixas()
+        
     }
 }
 </script>
--- src/components/Modal/ModalCaixa.vue
+++ src/components/Modal/ModalCaixa.vue
@@ -40,7 +40,7 @@
                             <q-item-section>Abertura/Fechamento</q-item-section>
                         </q-item>
 
-                        <q-item @click="clicavel('movimento')" :active="nameRouter === 'movimento'" clickable v-ripple>
+                        <q-item @click="abreDadosCaixa" :active="nameRouter === 'movimento'" clickable v-ripple>
                             <q-item-section avatar>
                                 <q-icon name="price_check" />
                             </q-item-section>
@@ -65,37 +65,157 @@
 
             <q-page-container>
                 <q-page>
+                    <div class="col-md-6 row justify-end items-center q-pa-sm">
+                        <hr class="bg-primary linhaInclusao">
+                        <q-btn @click="abreModalCadastro" flat round dense icon="add" color="white" class="bg-primary justify-start" />
+                    </div>
                     <CadastroCaixa v-if="nameRouter === 'cadastrocaixa'" />
                 </q-page>
             </q-page-container>
         </q-layout>
+        <q-dialog v-model="statusModalCadastro">
+            <q-card>
+                <q-toolbar>
+                    <q-toolbar-title>Cadastrar novo caixa</q-toolbar-title>
+                    <q-btn @click="fechaModalCadastro" flat round dense icon="close" v-close-popup />
+                </q-toolbar>
+
+                <q-card-section>
+                    <q-input :error="erroNomeCaixa" :error-message="erroMsgNomeCaixa" outlined v-model="nomeCaixa" label="Nome do caixa" stack-label dense class="q-mb-xs" />
+                    <q-input :error="erroObsCaixa" :error-message="erroMsgObsCaixa" outlined v-model="observacaoCaixa" label="Observação" stack-label dense class="q-mb-sm" />
+                    <div class="column items-center">
+                        <q-btn @click="salvaNovoCaixa" align="center" color="green" icon="add" label="Salvar" size="12px" />
+                    </div>
+                </q-card-section>
+            </q-card>
+        </q-dialog>
+        <ModalDadosCaixa ref="modalDadosCaixa" />
     </q-dialog>
 </template>
 
 <script>
 import CadastroCaixa from './FluxoCaixa/CadastroCaixa.vue'
+import ModalDadosCaixa from './ModalDadosCaixa.vue'
+import {Post} from 'src/utils/Conexao.js'
 
 export default {
-  components: { CadastroCaixa },
+  components: { CadastroCaixa, ModalDadosCaixa },
   data () {
     return {
-      drawer: false,
-      exibeModal: false,
-      miniState: true,
-      nameRouter: 'cadastrocaixa'
+        drawer: false,
+        exibeModal: false,
+        miniState: true,
+        statusModalCadastro: false,
+        erroNomeCaixa: false,
+        erroObsCaixa: false,
+        erroMsgNomeCaixa: '',
+        erroMsgObsCaixa: '',
+        nomeCaixa: '',
+        observacaoCaixa: '',
+        nameRouter: 'cadastrocaixa'
     }
   },
   methods : {
+    abreModalCadastro() {
+        this.statusModalCadastro = !this.statusModal
+    },
     exibirModal() {
         this.exibeModal = !this.exibeModal
+    },
+    confirmacao(mensagem) {
+        return new Promise((resolve, reject) => {
+            this.$q.dialog({
+                dark: true,
+                title: mensagem,
+                cancel: 'Sair',
+                persistent: true
+            }).onOk(() => {
+                resolve()
+            })
+        })
     },
     fechaModal() {
         this.exibeModal = !this.exibeModal
         this.nameRouter = null
     },
+    fechaModalCadastro() {
+        this.statusModalCadastro = !this.statusModalCadastro
+        this.nomeCaixa = null
+        this.observacaoCaixa = null
+    },
+    salvaNovoCaixa() {
+        this.validaCampos().then(() => {
+            this.confirmacao('Deseja incluir este novo caixa ?').then(() => {
+                var caixa = Object()
+                caixa.Nome = this.nomeCaixa
+                caixa.Observacao = this.observacaoCaixa
+
+                this.$q.notify({
+                    message: 'Carregando...',
+                    color: 'blue',
+                    timeout: 1000
+                })
+
+                setTimeout(() => {
+                    Post('v1/caixaoperador/cadastro', caixa).then(res => {
+                        if (res.data.status == true) {
+                            this.$q.notify({
+                                message: res.data.msg,
+                                color: 'green',
+                                timeout: 2000
+                            })
+                            this.fechaModalCadastro()
+                        } else {
+                            this.$q.notify({
+                                message: res.data.msg,
+                                color: 'red',
+                                timeout: 2000
+                            })
+                        }
+                    }).catch(err => {
+                        this.$q.notify({
+                            message: 'Erro',
+                            caption: 'Não foi possível fazer inclusão, tente novamente mais tarde',
+                            color: 'red'
+                        })
+                        console.log(err)
+                    })
+                }, 1000);
+            })
+        })
+    },
     clicavel(router) {
         this.nameRouter = router
-    }
+    },
+    abreDadosCaixa() {
+        this.$refs.modalDadosCaixa.exibirModal()
+    },
+    validaCampos() {
+        return new Promise(resolve => {
+            var campoVazio = false
+            var texto = 'Este campo é obrigatório preencher'
+
+            if (this.nomeCaixa === '' || this.nomeCaixa === null) {
+                this.erroNomeCaixa = true
+                this.erroMsgNomeCaixa = texto
+                campoVazio = true
+            } else {
+                this.erroNomeCaixa = false
+            }
+
+            if (this.observacaoCaixa === '' || this.observacaoCaixa === null) {
+                this.erroObsCaixa = true
+                this.erroMsgObsCaixa = texto
+                campoVazio = true
+            } else {
+                this.erroObsCaixa = false
+            }
+
+            if (campoVazio === false) {
+                resolve(true)
+            }
+        })
+    },
   }
 }
 </script>
